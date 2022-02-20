edb76a4e1b7bd15d77cf8ef853ec304f6bfc1590
маденечка гончаров <madene4ka@outlook.com>
2022-02-20 20:52:40 +0200

    Commit

--- CRUDCollection/Program.cs
+++ CRUDCollection/Program.cs
@@ -4,9 +4,79 @@ namespace CRUDCollection
 {
     internal class Program
     {
+        void PrintMenu()
+        {
+            Console.WriteLine("Choose from option");
+            Console.WriteLine("1. Add Doctors");
+            Console.WriteLine("2. Print All Doctors");
+            Console.WriteLine("3. Print one doctor");
+            Console.WriteLine("4. Edit doctor speciality");
+            Console.WriteLine("5. Edit doctor Experiance");
+            Console.WriteLine("6. Delete doctor");
+            Console.WriteLine("7. Sort and print");
+            Console.WriteLine("8. Exit");
+        }
+        void InteractWithDoctors()
+        {
+            Clinic manage = new Clinic();
+            int choice = 8;
+            do
+            {
+                PrintMenu();
+                while (!int.TryParse(Console.ReadLine(), out choice))
+                {
+                    Console.WriteLine("Invalid entry. Please try again");
+                }
+                switch (choice)
+                {
+                    case 1:
+                        string answer;
+                        do
+                        {
+                            manage.AddDoctor();
+                            Console.WriteLine("Do you want add onee more doctor. Enter no to finish");
+                            answer = Console.ReadLine();
+                        } while (answer != "no");
+                        break;
+
+                    case 2:
+                        manage.PrintAllDoctors();
+                        break;
+
+                    case 3:
+                        manage.PrintDoctorById();
+                        break;
+
+                    case 4:
+                        manage.EditSpeciality();
+                        break;
+
+                    case 5:
+                        manage.EditExperiance();
+                        break;
+
+                    case 6:
+                        manage.DeleteDoctor();
+                        break;
+
+                    case 7:
+                        manage.SortDoctorsBySpeciality();
+                        break;
+
+                    case 8:
+                        Console.WriteLine("Have a nice day ... Bye...");
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice. Try again");
+                        break;
+                }
+            } while (choice != 8);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program program = new Program();
+            program.InteractWithDoctors();
         }
     }
 }
